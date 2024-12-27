88bea79875a51f918adde69b4f07f268fe7690d5
Johannes Pallo <pallo.johannes@gmail.com>
2024-12-28 00:36:46 +0200

    add week 6 files

---
+++ app.js
@@ -0,0 +1,30 @@
+const userInputField = document.querySelector("#textInput");
+const addBtn = document.querySelector("#addBtn");
+const heroesDisplay = document.querySelector(".outputList");
+let heroes = [];
+
+addBtn.addEventListener("click", (event) => {
+    const userHero = userInputField.value;
+
+    if(userHero !== ''){heroes.push(userHero);
+
+
+    userInputField.value='';
+    heroesDisplay.innerHTML = '';
+    
+    heroes.forEach(hero => {
+
+        const heroListElement = document.createElement('li')
+        heroListElement.innerText = hero;
+        heroesDisplay.appendChild(heroListElement);
+
+    })
+    
+
+    } else {
+        console.log("Error, blank value")
+    }
+    
+    
+    event.preventDefault()
+});
